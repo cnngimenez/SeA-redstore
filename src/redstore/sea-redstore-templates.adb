25a9f1b71b52b3da30434bb6dadeb8def32e1057
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-01-17 15:40:14 -0300

    Library to use Turtle templates.

---
+++ src/redstore/sea-redstore-templates.adb
@@ -0,0 +1,169 @@
+--  sea-redstore-templates.adb ---
+
+--  Copyright 2020 cnngimenez
+--
+--  Author: cnngimenez
+
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------
+
+with Ada.Text_IO;
+with GNAT.Regpat;
+
+package body SeA.Redstore.Templates is
+
+    procedure Add_Value (Template : in out Template_Type;
+                         Key : Unbounded_String;
+                         Value : Unbounded_String) is
+    begin
+        Template.Fields.Include (Key, Value);
+    end Add_Value;
+
+    procedure Add_Value (Template : in out Template_Type;
+                         Key : String;
+                         Value : String) is
+    begin
+        Template.Add_Value (To_Unbounded_String (Key),
+                            To_Unbounded_String (Value));
+    end Add_Value;
+
+    function Apply (Template : Template_Type) return String is
+    begin
+        return To_String (Template.Apply);
+    end Apply;
+
+    function Apply (Template : Template_Type) return Unbounded_String is
+        Applied : Unbounded_String := Template.Text;
+        use String_Hash_Pack;
+
+        procedure Replace_String (Position : Cursor);
+
+        procedure Replace_String (Position : Cursor) is
+            Field_Name : constant String := To_String (Key (Position));
+            Value : constant String := To_String (Element (Position));
+            Field : constant String := "|" & Field_Name & "|";
+            Field_Position : Natural := 0;
+        begin
+            Field_Position := Index (Applied, Field, 1);
+            while Field_Position > 0 loop
+                Replace_Slice (Applied,
+                               Field_Position,
+                               Field_Position + Field'Length,
+                               Value);
+                Field_Position := Index (Applied, Field,
+                                         Field_Position + Field'Length);
+            end loop;
+        end Replace_String;
+
+    begin
+        Template.Fields.Iterate (Replace_String'Access);
+
+        return Applied;
+    end Apply;
+
+    procedure Fill_Fields (Template : in out Template_Type) is
+        use GNAT.Regpat;
+        Str : constant String := To_String (Template.Text);
+        Matches : Match_Array (0 .. 1);
+        Regexp : constant Pattern_Matcher :=
+          Compile (Field_Pattern);
+        Current : Natural := Str'First;
+        Empty_Str : constant Unbounded_String := To_Unbounded_String ("");
+        Field_Name : Unbounded_String;
+    begin
+        Template.Fields.Clear;
+        Ada.Text_IO.Put_Line (Str);
+
+        loop
+            Match (Regexp, Str, Matches, Current);
+            exit when Matches (0) = No_Match;
+
+            Ada.Text_IO.Put_Line
+              (Current'Image & ":"
+                 & Matches (0).First'Image & "-" & Matches (0).Last'Image);
+            Ada.Text_IO.Put_Line
+              (Current'Image & ":"
+                 & Matches (1).First'Image & "-" & Matches (1).Last'Image);
+
+            Field_Name := To_Unbounded_String
+              (Str (Matches (1).First .. Matches (1).Last));
+
+            Ada.Text_IO.Put_Line (To_String (Field_Name));
+
+            Template.Fields.Include (Field_Name, Empty_Str);
+
+            Current := Matches (0).Last + 1;
+        end loop;
+    end Fill_Fields;
+
+    function Get_Value (Template : Template_Type;
+                        Key : String) return String is
+
+    begin
+        return To_String (Template.Get_Value (To_Unbounded_String (Key)));
+    end Get_Value;
+
+    function Get_Value (Template : Template_Type;
+                        Key : Unbounded_String) return Unbounded_String is
+    begin
+        return Template.Fields.Element (Key);
+    end Get_Value;
+
+    procedure Initialize_With_File (Template : in out Template_Type;
+                                    Filepath : String) is
+
+        function Read_File (Filepath : String) return Unbounded_String;
+
+        function Read_File (Filepath : String) return Unbounded_String is
+            use Ada.Text_IO;
+            Temp : Unbounded_String;
+            File : File_Type;
+            Ch : Character;
+        begin
+            Open (File, In_File, Filepath);
+            loop
+                Get_Immediate (File, Ch);
+                exit when End_Of_File (File);
+                Append (Temp, Ch);
+            end loop;
+            Close (File);
+
+            return Temp;
+        end Read_File;
+
+    begin
+        Template.Text := Read_File (Filepath);
+        Fill_Fields (Template);
+    end Initialize_With_File;
+
+    procedure Iterate_Fields (Template : Template_Type;
+                              Proc : not null access
+                                procedure (Field_Name : String;
+                                           Field_Value : String)) is
+        use String_Hash_Pack;
+
+        procedure Call_Process (Position : Cursor);
+
+        procedure Call_Process (Position : Cursor) is
+        begin
+            Proc (To_String (Key (Position)),
+                  To_String (Element (Position)));
+        end Call_Process;
+
+    begin
+        Template.Fields.Iterate (Call_Process'Access);
+    end Iterate_Fields;
+
+end SeA.Redstore.Templates;
---
+++ src/redstore/sea-redstore-templates.ads
@@ -0,0 +1,71 @@
+--  sea-redstore-templates.ads ---
+
+--  Copyright 2020 cnngimenez
+--
+--  Author: cnngimenez
+
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
+with Ada.Containers.Hashed_Maps;
+with Ada.Strings.Unbounded.Hash;
+
+package SeA.Redstore.Templates is
+
+    type Template_Type is tagged private;
+
+    procedure Initialize_With_File (Template : in out Template_Type;
+                                    Filepath : String);
+    procedure Add_Value (Template : in out Template_Type;
+                         Key : Unbounded_String;
+                         Value : Unbounded_String);
+    procedure Add_Value (Template : in out Template_Type;
+                         Key : String;
+                         Value : String);
+    function Get_Value (Template : Template_Type;
+                        Key : String) return String;
+    function Get_Value (Template : Template_Type;
+                        Key : Unbounded_String) return Unbounded_String;
+    function Apply (Template : Template_Type) return String;
+    function Apply (Template : Template_Type) return Unbounded_String;
+
+    procedure Iterate_Fields (Template : Template_Type;
+                              Proc : not null access
+                                procedure (Field_Name : String;
+                                           Field_Value : String));
+
+private
+
+    package String_Hash_Pack is new Ada.Containers.Hashed_Maps
+      (
+       Key_Type => Unbounded_String,
+       Element_Type => Unbounded_String,
+       Hash => Ada.Strings.Unbounded.Hash,
+       Equivalent_Keys => "="
+      );
+
+    type Template_Type is tagged record
+        Text : Unbounded_String;
+        Fields : String_Hash_Pack.Map;
+    end record;
+
+    Field_Pattern : constant String := "\|([[:alnum:]]+)\|";
+
+    --  Reset Fields and add all "|fieldname|" strings founded to it.
+    procedure Fill_Fields (Template : in out Template_Type);
+
+end SeA.Redstore.Templates;
